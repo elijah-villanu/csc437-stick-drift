f5b7dbac55f2eac79e77cc950976cdc220fdecce
elijah-villanu <elijahred9@outlook.com>
2025-06-11 22:18:56 -0700

    Add DB Connections

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,16 +1,19 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import { connectMongo } from "mongoConnect";
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
 const STATIC_DIR = process.env.STATIC_DIR || "public";
 
+//Connect to database
+const mongoClient = connectMongo();
 
 const app = express();
 
 app.use(express.static(STATIC_DIR))
 
-app.get("/hello", (req: Request, res: Response) => {
+app.get("/api/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
 });
 
